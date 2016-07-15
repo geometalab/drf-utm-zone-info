e2ee781806a7ee1c3ad366a50bef6573c38010ab
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-07-15 17:46:04 +0200

    refactoring: extract py.test fixture providing a GEOS geometry

--- tests/serializers_test.py
+++ tests/serializers_test.py
@@ -1,19 +1,24 @@
+import pytest
 from django.contrib.gis.geos import Point
 from rest_framework_gis.fields import GeoJsonDict
 
 from utm_zone_info.serializers import GeometrySerializer
 
 
-def test_serialize_point():
-    p = Point(5, -23)
-    native_input = dict(geom=p)
+def test_serialize_point(geos_geometry):
+    native_input = dict(geom=geos_geometry)
     representation = GeometrySerializer(native_input).data
     assert representation == {'geom': GeoJsonDict([('type', 'Point'), ('coordinates', (5.0, -23.0))])}
 
 
-def test_deserialize_point():
+def test_deserialize_point(geos_geometry):
     representation = {'geom': GeoJsonDict([('type', 'Point'), ('coordinates', (5.0, -23.0))])}
     serializer = GeometrySerializer(data=representation)
     assert serializer.is_valid()
     native_output = serializer.validated_data
-    assert native_output == dict(geom=Point(5, -23))
+    assert native_output == dict(geom=geos_geometry)
+
+
+@pytest.fixture
+def geos_geometry():
+    return Point(5, -23)
