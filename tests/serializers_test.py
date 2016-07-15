c298e9675d80772cd202d4f7e5567594b90c6b50
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-07-15 17:58:09 +0200

    refactoring: extract py.test fixture providing the serialized representation of a GEOS geometry
    (as structure of Python native datatypes)

--- tests/serializers_test.py
+++ tests/serializers_test.py
@@ -5,14 +5,14 @@ from rest_framework_gis.fields import GeoJsonDict
 from utm_zone_info.serializers import GeometrySerializer
 
 
-def test_serialize_point(geos_geometry):
+def test_serialize_point(geos_geometry, geos_geometry_representation):
     native_input = dict(geom=geos_geometry)
     representation = GeometrySerializer(native_input).data
-    assert representation == {'geom': GeoJsonDict([('type', 'Point'), ('coordinates', (5.0, -23.0))])}
+    assert representation == geos_geometry_representation
 
 
-def test_deserialize_point(geos_geometry):
-    representation = {'geom': GeoJsonDict([('type', 'Point'), ('coordinates', (5.0, -23.0))])}
+def test_deserialize_point(geos_geometry, geos_geometry_representation):
+    representation = geos_geometry_representation
     serializer = GeometrySerializer(data=representation)
     assert serializer.is_valid()
     native_output = serializer.validated_data
@@ -22,3 +22,8 @@ def test_deserialize_point(geos_geometry):
 @pytest.fixture
 def geos_geometry():
     return Point(5, -23)
+
+
+@pytest.fixture
+def geos_geometry_representation():
+    return {'geom': GeoJsonDict([('type', 'Point'), ('coordinates', (5.0, -23.0))])}
