125dc07832f1da8327f0dd7d6bd6234c713dae1f
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-07-15 17:42:53 +0200

    add test for geometry deserialization

--- tests/serializers_test.py
+++ tests/serializers_test.py
@@ -9,3 +9,11 @@ def test_serialize_point():
     native_input = dict(geom=p)
     representation = GeometrySerializer(native_input).data
     assert representation == {'geom': GeoJsonDict([('type', 'Point'), ('coordinates', (5.0, -23.0))])}
+
+
+def test_deserialize_point():
+    representation = {'geom': GeoJsonDict([('type', 'Point'), ('coordinates', (5.0, -23.0))])}
+    serializer = GeometrySerializer(data=representation)
+    assert serializer.is_valid()
+    native_output = serializer.validated_data
+    assert native_output == dict(geom=Point(5, -23))
