96308a9506e522145363a33d0b7e047cd51bc830
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-07-16 12:41:37 +0200

    let a bare GEOS geometry be the native value of the GeometrySerializer
    rather than the geometry wrapped in a single-entry dict

--- tests/serializers_test.py
+++ tests/serializers_test.py
@@ -6,7 +6,7 @@ from utm_zone_info.serializers import GeometrySerializer
 
 
 def test_serialize_point(geos_geometry, geos_geometry_representation):
-    native_input = dict(geom=geos_geometry)
+    native_input = geos_geometry
     representation = GeometrySerializer(native_input).data
     assert representation == geos_geometry_representation
 
@@ -16,7 +16,7 @@ def test_deserialize_point(geos_geometry, geos_geometry_representation):
     serializer = GeometrySerializer(data=representation)
     assert serializer.is_valid()
     native_output = serializer.validated_data
-    assert native_output == dict(geom=geos_geometry)
+    assert native_output == geos_geometry
 
 
 @pytest.fixture
