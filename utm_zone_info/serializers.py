55997ba3562fe45811f514817748d9aa73e2931e
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-07-16 12:41:37 +0200

    let a bare GEOS geometry be the native value of the GeometrySerializer
    rather than the geometry wrapped in a single-entry dict

--- utm_zone_info/serializers.py
+++ utm_zone_info/serializers.py
@@ -4,3 +4,9 @@ from rest_framework_gis.fields import GeometryField
 
 class GeometrySerializer(serializers.Serializer):
     geom = GeometryField()
+
+    def to_internal_value(self, data):
+        return super(GeometrySerializer, self).to_internal_value(data)['geom']
+
+    def to_representation(self, instance):
+        return super(GeometrySerializer, self).to_representation(dict(geom=instance))
