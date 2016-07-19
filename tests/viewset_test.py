e009e97121e03df58231f7601e496af1da82713f
Nicola Jordan <nic@hixi.ch>
2016-07-19 14:45:18 +0200

    also test invalid payloads

--- tests/viewset_test.py
+++ tests/viewset_test.py
@@ -1,6 +1,7 @@
 from unittest import mock
 
 import pytest
+from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APIClient
 
@@ -13,27 +14,46 @@ def test_posting_valid_data_returns_utm_zones(api_client, utm_zone_post_url, pay
             'utm_zone_info.viewsets.utm_zones_for_representing', return_value=[utm_zone_mock]):
         post_viewset_result = api_client.post(utm_zone_post_url, payload, format='json')
         expected_result = {'utm_zone_srids': [expected_srid]}
+        assert post_viewset_result.status_code == status.HTTP_200_OK
         assert post_viewset_result.json() == expected_result
 
 
-@pytest.fixture
-def valid_geoJSON():
-    return {
-        "type": "Polygon",
-        "coordinates": [
-            [
-                [100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0]
-            ]
+def test_posting_invalid_data_returns_error(api_client, utm_zone_post_url, invalid_payload):
+    post_viewset_result = api_client.post(utm_zone_post_url, invalid_payload, format='json')
+    assert post_viewset_result.status_code == status.HTTP_400_BAD_REQUEST
+
+
+_valid_geoJSON = {
+    "type": "Polygon",
+    "coordinates": [
+        [
+            [100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0]
         ]
-    }
+    ]
+}
 
 
-@pytest.fixture
-def payload(valid_geoJSON):
-    return {
-        "srid": 4326,
-        'geom': valid_geoJSON,
-    }
+@pytest.fixture(params=[
+    dict(),
+    dict(geom=''),
+    dict(srid=None),
+    dict(geom=None, srid=None),
+    dict(geom='', srid=None),
+    dict(geom=None, srid=''),
+    dict(geom='', srid=4326),
+    dict(geom=_valid_geoJSON, srid=''),
+    dict(geom=_valid_geoJSON.copy().pop('type'), srid=4326),
+    dict(geom=_valid_geoJSON.copy().pop('coordinates'), srid=4326),
+])
+def invalid_payload(request):
+    return request.param
+
+
+@pytest.fixture(params=[
+    dict(geom=_valid_geoJSON, srid=4326)
+])
+def payload(request):
+    return request.param
 
 
 @pytest.fixture
