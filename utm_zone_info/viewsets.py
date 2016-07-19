27430bd31e4269a5fdd2ac516f2d907110edfd26
Nicola Jordan <nic@hixi.ch>
2016-07-19 11:18:45 +0200

    add basic url patterns

---
+++ utm_zone_info/viewsets.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+
+
+class UTMZoneInfoViewSet(viewsets.ViewSet):
+    """
+    A simple ViewSet for posting Points and returns valid utm_zones.
+    """
+
+    def create(self, request):
+        pass
