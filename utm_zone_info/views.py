2bd2757ff86f59902f90781f21944a47dfe185aa
Nicola Jordan <njordan.hsr@gmail.com>
2016-08-30 16:43:10 +0200

    refactoring: use class based view for easier extension

--- utm_zone_info/views.py
+++ utm_zone_info/views.py
@@ -1,25 +1,27 @@
+from rest_framework import permissions
 from rest_framework import status
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from utm_zone_info.coordinate_reference_system import utm_zones_for_representing
 from utm_zone_info.serializers import GeometrySerializer
 
 
-@api_view(['POST'])
-def utm_zone_info(request):
+class UTMZoneSRIDView(APIView):
     """
     A simple View accepting a geometry and returning SRIDs of UTM Zones that can represent this geometry.
     """
+    permission_classes = (permissions.AllowAny,)
 
-    # only POST allowed, so no need to check for "request.method == 'POST'"
-    serializer = GeometrySerializer(data=request.data)
-    if serializer.is_valid():
-        geometry = serializer.validated_data['geom']
-        geometry.srid = serializer.validated_data['srid']
-        data = dict(
-            utm_zone_srids=[zone.srid for zone in utm_zones_for_representing(geometry)]
-        )
-        return Response(data=data)
-    else:
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, *args, **kwargs):
+        serializer = GeometrySerializer(data=request.data)
+        if serializer.is_valid():
+            geometry = serializer.validated_data['geom']
+            geometry.srid = serializer.validated_data['srid']
+            data = dict(
+                utm_zone_srids=[zone.srid for zone in utm_zones_for_representing(geometry)]
+            )
+            return Response(data=data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+utm_zone_info = UTMZoneSRIDView.as_view()
